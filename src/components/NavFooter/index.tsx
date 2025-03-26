27d0269300a70f4d5bcc9375fbb5f337463a457e
doraemon <doraemon@etransfer.exchange>
2025-03-26 19:01:35 +0800

    feat: update footer power text

--- src/components/NavFooter/index.tsx
+++ src/components/NavFooter/index.tsx
@@ -142,9 +142,7 @@ export default function NavFooter({ className, style, footerMediaClassName, data
               );
             })}
           </div>
-          <div className={styles.footerPower}>
-            {data.powerName.text}@{getFullYear()}
-          </div>
+          <div className={styles.footerPower}>{`Copyright © ${getFullYear()} ${data.powerName.text}`}</div>
         </div>
       </div>
     </footer>
